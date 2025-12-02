b215895c988d44d3feb024cc1738920040b0a12c
guptarudraksh <guptarudraksh6@gmail.com>
2025-12-03 05:29:20 +0530

    first commit

---
+++ compund_interest_calculator.py
@@ -0,0 +1,21 @@
+principle = 0
+rate = 0
+time = 0
+
+while principle <=0:
+    principle = float(input("Enter the principal amount: "))
+    if principle <= 0:
+        print("Principal can't be less than or equal to zero")
+
+while rate <=0:
+    rate = float(input("Enter the interest rate: "))
+    if rate <= 0:
+        print("Interest rate can't be less than or equal to zero")
+
+while time <=0:
+    time = int(input("Enter the time in years: "))
+    if time <= 0:
+        print("Time can't be less than or equal to zero")
+
+total = principle * pow((1 + rate / 100),time)
+print(f"balance after {time} year/s: ${total:.2f}")
